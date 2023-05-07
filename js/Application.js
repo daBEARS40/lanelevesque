42f4c4520378640e959966fa9ddf680347b37034
Lane Levesque <lane.levesque@hotmail.com>
2023-05-06 17:10:54 -0700

    commit without vid, css and js progress

--- js/Application.js
+++ js/Application.js
@@ -1,10 +1,29 @@
-define([], () => {
+define(["jquery"], ($) => {
   class Application {
-    constructor() {}
-
-    start() {
-      console.log("started!");
+    constructor(options) {
+      this.mode = options.mode || "mint";
     }
+
+    init() {
+      ApplyUIBindings.call(this);
+    }
+  }
+
+  function ApplyUIBindings() {
+    $('#header-mint').on('click', ShowPage.bind(this, "mint"));
+    $('#header-about').on('click', ShowPage.bind(this, "about"));
+    $('#header-white').on('click', ShowPage.bind(this, "white"));
+    $('#header-learn').on('click', ShowPage.bind(this, "learn"));
+    $('#header-benefits').on('click', ShowPage.bind(this, "benefits"));
+    $('#header-merch').on('click', ShowPage.bind(this, "merch"));
+  }
+
+  function ShowPage(mode) {
+    this.mode = mode;
+    $.each($("#centerbox").children(), (_i, v) => {
+      $(v).hide();
+      if (v.id == mode) $(v).show();
+    });
   }
 
   return Application;
